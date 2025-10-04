b8cf81b0c493db050d04d99898457a305a54bd22
LucasFe3152 <167145993+LucasFe3152@users.noreply.github.com>
2025-10-04 20:07:35 -0300

    work at embeddings.py

--- app/utils/embeddings.py
+++ app/utils/embeddings.py
@@ -2,6 +2,7 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
+import ast
 
 class EmbeddingEngine:
     def __init__(self, model_name='all-MiniLM-L6-v2'):
@@ -41,21 +42,51 @@ class EmbeddingEngine:
             return [0]
 
 
-# Exemplo de uso
+def safe_embedding_converter(value):
+    if isinstance(value, str):
+        try:
+            return np.array(ast.literal_eval(value))
+        except (ValueError, SyntaxError):
+            print(f'Erro no valor: {value}')
+            return np.array([0.0])
+    else:
+        return np.array([value])
+
+def calculate_similarity(csv_path: str ,engine: EmbeddingEngine) -> list:
+    df = pd.read_csv(csv_path)
+
+    df['embedding'] = df['embedding'].apply(safe_embedding_converter)
+
+    n = len(df)
+    similarity = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i1, row1 in df.iterrows():
+        for i2, row2 in df.iterrows():
+            
+            sim = engine.compare_embeddings(row1['embedding'], row2['embedding'])
+            similarity[i1][i2] = sim
+            similarity[i2][i1] = sim
+    
+    return similarity
+
+
 if __name__ == "__main__":
-    # engine = EmbeddingEngine()
     # embeddings = engine.read_csv('app/utils/test_publications.csv', 'Abstract')
     # similarity = engine.compare_embeddings(embeddings[1], embeddings[2])
     # print(embeddings[1], embeddings[2])
     # print(f"Similaridade: {similarity:.4f}")
-    df = pd.read_csv('app/utils/artigos.csv')
-    df['embeddings'] = [[] for _ in range(len(df))]
-    df['embeddings'] = df['embeddings'].astype(object)
+    # df = pd.read_csv('app/utils/artigos.csv')
+    # df['embeddings'] = [[] for _ in range(len(df))]
+    # df['embeddings'] = df['embeddings'].astype(object)
+    # engine = EmbeddingEngine()
+    # for i, r in df.iterrows():
+    #     if i == 5:
+    #         break
+    #     emb = engine.calculate_embeddings(r, 'abstract')
+    #     df.at[i, 'embeddings'] = emb
+    #     # print(type(r))  
+    # print(df['embeddings'].head())
     engine = EmbeddingEngine()
-    for i, r in df.iterrows():
-        if i == 5:
-            break
-        emb = engine.calculate_embeddings(r, 'abstract')
-        df.at[i, 'embeddings'] = emb
-        # print(type(r))  
-    print(df['embeddings'].head())
+    s = calculate_similarity('app/utils/artigos_embeddings.csv', engine)
+    for i in range(5):
+        print(s[i])
