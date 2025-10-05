37b903f588ed663dd7ee52b1d77cb90482bbbe58
Gabryel <gabryel28cardoso@gmail.com>
2025-10-05 16:38:19 -0300

    configurando limite de proximidade 3

--- app/services/graph_service.py
+++ app/services/graph_service.py
@@ -55,7 +55,7 @@ def edge_generator():
     json_data = read_json("similarity.json")
     for i in range(len(json_data)):
         for j in range(len(json_data[i])):
-            if json_data[i][j] < 0.83:
+            if json_data[i][j] < 0.83  or json_data[i][j] >= 0.999 :
                continue
             edge = {
                 "source": i,
